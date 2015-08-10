cde573a6cb139f0c36867796388ae78f6d6fd304
Jordan Anslow <jordananslow@gmail.com>
2015-08-10 23:35:01 +1000

    Added step model + view + url

--- courses/urls.py
+++ courses/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.course_list,),
-    url(r'(?P<pk>\d+)/$', views.course_detail) # Searches for /courses/n
+    url(r'(?P<course_pk>\d+)/(?P<step_pk>\d+)/$', views.step_detail), # Above courses so that we don't accidently accept below
+    url(r'(?P<pk>\d+)/$', views.course_detail), # Searches for /courses/n
 ]
